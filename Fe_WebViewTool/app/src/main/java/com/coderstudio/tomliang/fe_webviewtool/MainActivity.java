66afa2e7c3f819ef404d91ff19689353ca0a2ed1
liang3472 <632585468@qq.com>
2016-08-03 19:17:44 +0800

    live.js注入,历史记录

--- Fe_WebViewTool/app/src/main/java/com/coderstudio/tomliang/fe_webviewtool/MainActivity.java
+++ Fe_WebViewTool/app/src/main/java/com/coderstudio/tomliang/fe_webviewtool/MainActivity.java
@@ -1,15 +1,14 @@
 package com.coderstudio.tomliang.fe_webviewtool;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,17 +20,20 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.coderstudio.tomliang.fe_webviewtool.bean.History;
+import com.coderstudio.tomliang.fe_webviewtool.dao.HistoryDao;
+import com.coderstudio.tomliang.fe_webviewtool.itf.HistoryOperationI;
 import com.coderstudio.tomliang.fe_webviewtool.utils.Utils;
 import com.coderstudio.tomliang.fe_webviewtool.zxing.CaptureActivity;
 
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends BaseActivity
+        implements NavigationView.OnNavigationItemSelectedListener, HistoryOperationI {
 
     @BindView(R.id.drawer_layout)
     protected DrawerLayout drawer;
@@ -47,18 +49,45 @@ public class MainActivity extends AppCompatActivity
     protected EditText etUrl;
     @BindView(R.id.progressbar)
     protected ProgressBar progressBar;
+    private HistoryDao historyDao;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+    protected int getContentView() {
+        return R.layout.activity_main;
+    }
 
+    @Override
+    protected void initView() {
+        historyDao = AppContext.getInstance().getDaoSession().getHistoryDao();
+        initActionBar();
+        initWebView();
+        navigationView.setNavigationItemSelectedListener(this);
+        btnGoTo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!etUrl.getText().toString().startsWith("http")){
+                    etUrl.setText("http://"+etUrl.getText().toString());
+                }
+
+                if (!TextUtils.isEmpty(etUrl.getText())) {
+                    if (Utils.isUrl(etUrl.getText().toString())) {
+                        goToUrl(etUrl.getText().toString());
+                    } else {
+                        Utils.toastMsg(v, "不是一个链接");
+                    }
+                }
+            }
+        });
+    }
+
+    private void initActionBar() {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+    }
 
+    private void initWebView() {
         wvContent.getSettings().setJavaScriptEnabled(true);
         wvContent.getSettings().setDomStorageEnabled(true);
         wvContent.setWebViewClient(new WebViewClient() {
@@ -75,8 +104,14 @@ public class MainActivity extends AppCompatActivity
                 if (etUrl != null) {
                     etUrl.setText(url);
                 }
-                view.loadUrl(Utils.getInjectErudaJs());
+                view.loadUrl(Utils.injectJsLib(Utils.getInjectErudaJs(), Utils.getInjectLiveJs()));
                 super.onPageFinished(view, url);
+            }
+
+            @Override
+            public void onLoadResource(WebView view, String url) {
+                super.onLoadResource(view, url);
+                Log.e("test", url);
             }
         });
 
@@ -92,21 +127,84 @@ public class MainActivity extends AppCompatActivity
             }
 
         });
+    }
 
-        btnGoTo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!TextUtils.isEmpty(etUrl.getText())) {
-                    if (Utils.isUrl(etUrl.getText().toString())) {
-                        wvContent.loadUrl(etUrl.getText().toString());
-                    } else {
-                        Utils.toastMsg(v, "不是一个链接");
-                    }
+    private void goToUrl(String url){
+        wvContent.loadUrl(url);
+        addHistory(url);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.qr_scan) {
+            startActivityForResult(new Intent(this, CaptureActivity.class), CaptureActivity.OPEN_QR);
+        } else if (id == R.id.checkWebGL) {
+            wvContent.loadUrl("file:///android_asset/testWebGL.html");
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_history) {
+            startActivityForResult(new Intent(this, HistoryActivity.class), HistoryActivity.OPEN_HISTORY);
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        switch (requestCode) {
+            case CaptureActivity.OPEN_QR:
+                //扫描二维码返回
+                if (null != data) {
+                    String url = data.getStringExtra(CaptureActivity.QR_RESULT);
+                    etUrl.setText(url);
+                    goToUrl(url);
                 }
-            }
-        });
+                break;
+            case HistoryActivity.OPEN_HISTORY:
+                //扫描二维码返回
+                if (null != data) {
+                    String url = data.getStringExtra(HistoryActivity.HISTORY_RESULT);
+                    etUrl.setText(url);
+                    goToUrl(url);
+                }
+                break;
+            default:
+                break;
+        }
 
-        navigationView.setNavigationItemSelectedListener(this);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void addHistory(String url) {
+        History history = new History(url);
+        List<History> histories = historyDao.queryBuilder().where(HistoryDao.Properties.Url.eq(url)).list();
+        if (histories != null && histories.size() > 0) {
+            History old = histories.get(0);
+            history.setCount(old.getCount() + 1);
+        } else {
+            history.setCount(1);
+        }
+        historyDao.insertOrReplace(history);
+    }
+
+    @Override
+    public List<History> getHistories() {
+        return null;
     }
 
     /**
@@ -147,52 +245,5 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.qr_scan) {
-            startActivityForResult(new Intent(this, CaptureActivity.class), CaptureActivity.OPEN_QR);
-        } else if (id == R.id.checkWebGL) {
-            wvContent.loadUrl("file:///android_asset/testWebGL.html");
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        switch (requestCode) {
-            case CaptureActivity.OPEN_QR:
-                //扫描二维码返回
-                if (null != data) {
-                    String url = data.getStringExtra(CaptureActivity.QR_RESULT);
-                    etUrl.setText(url);
-                    wvContent.loadUrl(url);
-                }
-                break;
-            default:
-                break;
-        }
-
-        super.onActivityResult(requestCode, resultCode, data);
-    }
 }
--- Fe_WebViewTool/app/src/main/java/com/coderstudio/tomliang/fe_webviewtool/utils/Utils.java
+++ Fe_WebViewTool/app/src/main/java/com/coderstudio/tomliang/fe_webviewtool/utils/Utils.java
@@ -3,6 +3,8 @@ package com.coderstudio.tomliang.fe_webviewtool.utils;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 
+import com.coderstudio.tomliang.fe_webviewtool.bean.JsLib;
+
 import java.util.regex.Pattern;
 
 /**
@@ -20,16 +22,35 @@ public class Utils {
         return pattern.matcher(strLink).matches();
     }
 
-    public static void toastMsg(View view, String msg){
+    public static void toastMsg(View view, String msg) {
         Snackbar.make(view, msg, Snackbar.LENGTH_SHORT).show();
     }
 
-    public static String getInjectErudaJs(){
-        String jsLib = "javascript:(function () { " +
-                "var script = document.createElement('script');" +
-                " script.src=\"//liriliri.github.io/eruda/eruda.min.js\";" +
-                " script.onload = function () { eruda.init(); };" +
-                " document.body.appendChild(script);})();";
-        return jsLib;
+    public static JsLib getInjectErudaJs() {
+        JsLib lib = new JsLib();
+        lib.setJsSrc("//liriliri.github.io/eruda/eruda.min.js");
+        lib.setJsOnLoad("function () { eruda.init(); }");
+        return lib;
+    }
+
+    public static JsLib getInjectLiveJs() {
+        JsLib lib = new JsLib();
+        lib.setJsSrc("http://livejs.com/live.js");
+        lib.setJsOnLoad("undefined");
+        return lib;
+    }
+
+    public static String injectJsLib(JsLib... libs) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("javascript:(function () { ");
+        for (int i = 0; i < libs.length; i++) {
+            sb.append("var script" + i + " = document.createElement('script');");
+            sb.append("script" + i + ".src=\"" + libs[i].getJsSrc() + "\";");
+            sb.append("script" + i + ".onload = " + libs[i].getJsOnLoad() + ";");
+            sb.append("document.body.appendChild(script" + i + ");");
+        }
+        sb.append("})();");
+
+        return sb.toString();
     }
 }
