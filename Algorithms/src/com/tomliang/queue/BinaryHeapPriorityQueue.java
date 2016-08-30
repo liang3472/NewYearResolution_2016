d0b4d6487457a2428d8cf98ddddee9276f9e9152
liang3472 <632585468@qq.com>
2016-08-30 18:59:23 +0800

    添加二叉堆优先队列定义

---
+++ Algorithms/src/com/tomliang/queue/BinaryHeapPriorityQueue.java
@@ -0,0 +1,44 @@
+package com.tomliang.queue;
+
+/**
+ * 
+ * @author lianghangbing
+ * <p>优先队列(二叉堆实现)</p>
+ * <p>原理:</p>
+ * <p></p>
+ * <p></p>
+ * @param <T>
+ */
+public class BinaryHeapPriorityQueue<T> implements IPriorityQueue<T> {
+
+	@Override
+	public void insert(T t) {
+		
+	}
+
+	@Override
+	public T delMin() {
+		return null;
+	}
+
+	@Override
+	public T min() {
+		return null;
+	}
+
+	@Override
+	public int size() {
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return false;
+	}
+
+	@Override
+	public boolean less(T t1, T t2) {
+		return false;
+	}
+
+}
